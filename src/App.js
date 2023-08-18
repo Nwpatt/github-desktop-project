74270bb9f3796b42b608964a8aedaa05241939c9
Nwpatt <nawapat_link@hotmail.com>
2023-08-18 14:57:17 +0700

    banner and calculator

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Banner from "./banner";
+import { Calculator2 } from "./calculator";
 import { Header,Content,Footer } from "./func-components";
 import Calendar from "./class-components";
 
@@ -8,8 +10,11 @@ function App() {
       <Header/>
       <p><center><Calendar/></center></p>
       <Content/>
+      <Banner/>
+      <Calculator2/>
       <Footer/>
     </>
   )
 }
+
 export default App
