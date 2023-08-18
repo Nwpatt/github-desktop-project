7ea31cfd4147771153028105f8b7059bb9665882
Nwpatt <nawapat_link@hotmail.com>
2023-08-18 13:54:07 +0700

    new update

--- src/func-components.js
+++ src/func-components.js
@@ -1,38 +1,35 @@
-import React, { Component } from "react";
+import React from 'react'
 
-export default class Calendar extends Component {
-  getDate() {
-    const dayNames = [
-      "อาทิตย์",
-      "จันทร์",
-      "อังคาร",
-      "พุธ",
-      "พฤหัสบดี",
-      "ศุกร์",
-      "เสาร์",
-    ];
-    const monthNames = [
-      "มกราคม",
-      "กุมภาพันธ์",
-      "มีนาคม",
-      "เมษายน",
-      "พฤษภาคม",
-      "มิถุนายน",
-      "กรกฎาคม",
-      "สิงหาคม",
-      "กันยายน",
-      "ตุลาคม",
-      "พฤศจิกายน",
-      "ธันวาคม",
-    ];
-    const date = new Date();
-    const weekDay = dayNames[date.getDay()];
-    const day = date.getDate();
-    const month = monthNames[date.getMonth()];
-    const year = date.getFullYear() + 543;
-    return `วัน${weekDay} ที่ ${day} ${month} ${year}`;
-  }
-  render() {
-    return <div>{this.getDate()}</div>;
-  }
+export function Header(){
+    const aStyle ={
+        display: 'inline-block',
+        color: 'blue',
+        margin: '7px',
+        TextDecoration: 'none'
+    }
+
+    return (
+    <div style={{backgroundColor:'#ccc'}}>
+        <a href=" "style={aStyle}>Home</a> |&nbsp;
+        <a href=" "style={aStyle}>React</a> |&nbsp;
+        <a href=" "style={aStyle}>React Native</a> 
+    </div>
+)
+}
+
+    export const Footer = () => {
+    return (
+        <div style={{textAlign:'center'}}>
+            &copy;{new Date().getFullYear()} All rights reserved
+        </div>
+    )
+}
+
+export function Content(){
+    return (
+        <div style={{textAlign:'center'}}>
+            <h2>Hello<br/>React & React Native</h2>
+            <br/><br/><br/>
+        </div>
+    )
 }
