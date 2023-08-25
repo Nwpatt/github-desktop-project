af63e9a56ab15b69458e9c6f928a9f23c4d4ab24
Nwpatt <nawapat_link@hotmail.com>
2023-08-25 15:04:55 +0700

    refs array

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from "react";
-import Banner from "./banner";
-import { Calculator2 } from "./calculator";
-import { EventData2 } from "./event-data";
-import { Header,Content,Footer } from "./func-components";
-import RefsFunc from "./refs-func";  
-import Calendar from "./class-components";
+// import Banner from "./banner";
+// import { Calculator2 } from "./calculator";
+// import { EventData2 } from "./event-data";
+// import { Header,Content,Footer } from "./func-components";
+// import RefsFunc from "./refs-func";  
+// import Calendar from "./class-components";
+import RefsArray from "./refs-array";
 
 function App() {
   return (
     <>
-      <Header/>
+      {/* <Header/>
       <p><center><Calendar/></center></p>
       <Content/>
       <Banner/>
       <Calculator2/>
       <EventData2/>
-      <RefsFunc/>
-      <Footer/>
+      <RefsFunc/> */}
+      <RefsArray/>
+      {/* <Footer/> */}
     </>
   )
 }
