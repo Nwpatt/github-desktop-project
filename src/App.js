ab1f26884f696c14ed78ea1651cf7723c5e276df
Nwpatt <nawapat_link@hotmail.com>
2023-09-22 15:15:42 +0700

    new from 22-9-2023

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+// import Router2 from "./router2";
+import Router3 from "./router3";
 // import Banner from "./banner";
 // import { Calculator2 } from "./calculator";
 // import { EventData2 } from "./event-data";
@@ -7,14 +9,14 @@ import React from "react";
 // import Calendar from "./class-components";
 // import RefsArray from "./refs-array";
 // import MessageBox from "./state-func";
-import { userContext } from './context';
+// import { userContext } from './context';
 // import Header from "./context-header";
 // import Content from "./context-content";
-import Header2 from "./context-header2";
-import Content2 from "./context-content2";
+// import Header2 from "./context-header2";
+// import Content2 from "./context-content2";
 
 function App() {
-  let [user,setUser] = React.useState()
+  // let [user,setUser] = React.useState()
   return (
     <>
       {/* <Header/>
@@ -33,10 +35,11 @@ function App() {
         <Content/>
       </userContext.Provider> */}
       
-      <userContext.Provider value={[user,setUser]}>
+      {/* <userContext.Provider value={[user,setUser]}>
         <Header2/>
         <Content2/>
-      </userContext.Provider>
+      </userContext.Provider> */}
+      <Router3/>
     </>
   )
 }
